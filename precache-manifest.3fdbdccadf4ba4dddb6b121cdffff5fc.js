13438f24f80fce11294448c8f9d7b8ac36348eb7
PoserJS <poserjs@gmail.com>
2022-03-12 13:09:23 -0800

    Added zone statistics

---
+++ precache-manifest.3fdbdccadf4ba4dddb6b121cdffff5fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8cf0e9e54135205b00cf828df6b0cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70a8be1268ac521d0a78",
+    "url": "/static/css/2.4cb84118.chunk.css"
+  },
+  {
+    "revision": "656915c2401a73873f28",
+    "url": "/static/css/main.e6e0c017.chunk.css"
+  },
+  {
+    "revision": "70a8be1268ac521d0a78",
+    "url": "/static/js/2.0a93b3f6.chunk.js"
+  },
+  {
+    "revision": "eca2427c5bac0a08d7690075dd259ad4",
+    "url": "/static/js/2.0a93b3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "656915c2401a73873f28",
+    "url": "/static/js/main.1272cffa.chunk.js"
+  },
+  {
+    "revision": "ad5c480a0a6294198108",
+    "url": "/static/js/runtime-main.ba368b7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d82fad19f3705dbd6d091283fb85b16.js"
+  "/precache-manifest.3fdbdccadf4ba4dddb6b121cdffff5fc.js"
 );
 
 self.addEventListener('message', (event) => {
